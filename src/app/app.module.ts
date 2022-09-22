190d0bbe626ba84167fcf848003d6ec21c223c8f
Gaiaz65 <gayazapkaev365@gmail.com>
2022-09-22 16:55:41 +0300

    Created necessary components. Figured out the logic. Added navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { ResumeComponent } from './resume/resume.component';
+import { ProjectComponent } from './projects/list/project/project.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ContactsComponent,
+    HomeComponent,
+    NavigationComponent,
+    SidebarComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MaterialModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
